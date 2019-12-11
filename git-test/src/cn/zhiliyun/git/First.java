6f23b533a53188543b97b5aa5d61f77f3743f806
lym <547482051@qq.com>
2019-12-11 10:36:20 +0800

    删除

--- git-test/src/cn/zhiliyun/git/First.java
+++
@@ -1,9 +0,0 @@
-package cn.zhiliyun.git;
-
-public class First {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-    }
-}
