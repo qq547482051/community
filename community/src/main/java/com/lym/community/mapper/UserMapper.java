63dd76182a7c0718c6d6ea24180286d69ab14b92
lym <547482051@qq.com>
2019-12-13 09:35:19 +0800

    添加问题发起页面

---
+++ community/src/main/java/com/lym/community/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.lym.community.mapper;
+
+
+import com.lym.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Created by codedrinker on 2019/4/30.
+ */
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+}
