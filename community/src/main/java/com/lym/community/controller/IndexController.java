531517c5b80c64f4abb36c8ea617d5f3ecfc6b9d
lym <547482051@qq.com>
2019-12-16 15:23:07 +0800

    我的问题功能

--- community/src/main/java/com/lym/community/controller/IndexController.java
+++ community/src/main/java/com/lym/community/controller/IndexController.java
@@ -23,24 +23,25 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-
-        Model model,
-        @RequestParam(name = "page", defaultValue = "1") Integer page,
-        @RequestParam(name = "size", defaultValue = "5") Integer size) {
-            Cookie[] cookies = request.getCookies();
-            if (cookies != null && cookies.length != 0)
-                for (Cookie cookie : cookies) {
-                    if (cookie.getName().equals("token")) {
-                        String token = cookie.getValue();
-                        User user = userMapper.findByToken(token);
-                        if (user != null) {
-                            request.getSession().setAttribute("user", user);
-                        }
-                        break;
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
+        Cookie[] cookies = request.getCookies();
+        User user = null;
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
                     }
+                    break;
                 }
-            PaginationDTO pagination = questionService.list(page, size);
-            model.addAttribute("pagination", pagination);
-            return "index";
+            }
         }
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination", pagination);
+        return "index";
     }
+}
