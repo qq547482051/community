414b497aabd8c4e2ce9afe6da72a570a91cd83a0
lym <547482051@qq.com>
2019-12-17 15:04:14 +0800

    问题详情页面

---
+++ community/src/main/java/com/lym/community/model/Question.java
@@ -0,0 +1,17 @@
+package com.lym.community.model;
+
+import lombok.Data;
+
+@Data
+public class Question {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmt_create;
+    private Long gmt_modified;
+    private Integer creator;
+    private Integer view_count;
+    private Integer like_count;
+    private Integer comment_count;
+}
