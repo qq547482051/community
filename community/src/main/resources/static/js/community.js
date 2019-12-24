a4e349dc6c43077c489c61a71499bc3a087bce46
lym <547482051@qq.com>
2019-12-24 16:13:12 +0800

    不刷新页面登录功能

--- community/src/main/resources/static/js/community.js
+++ community/src/main/resources/static/js/community.js
@@ -14,9 +14,16 @@ function post() {
             if(response.code == 200){
                 $("#comment_section").hide();
             }else {
-                alert(response.message);
+                if(response.code ==2003){
+                    var isAccepted = confirm(response.message);
+                    if(isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=d7afcf2bdb1c1f438cdd&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                }else {
+                    alert(response.message);
+                }
             }
-            console.log(response);
         },
         dataType:"json"
     });
