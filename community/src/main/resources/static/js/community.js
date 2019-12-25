1a36a47633348937e11356005ac3eaae9baf7a11
lym <547482051@qq.com>
2019-12-25 17:07:11 +0800

    二级评论页面设计

--- community/src/main/resources/static/js/community.js
+++ community/src/main/resources/static/js/community.js
@@ -1,34 +1,51 @@
+//提交回复
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
-    if(!content){
+    if (!content) {
         alert("不能回复空内容");
         return;
     }
     $.ajax({
-        type:"POST",
-        url:"/comment",
-        contentType:'application/json',
-        data:JSON.stringify({
-            "parent_id":questionId,
-            "content":content,
-            "type":1
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parent_id": questionId,
+            "content": content,
+            "type": 1
         }),
-        success:function (response) {
-            if(response.code == 200){
+        success: function (response) {
+            if (response.code == 200) {
                 window.location.reload();
-            }else {
-                if(response.code ==2003){
+            } else {
+                if (response.code == 2003) {
                     var isAccepted = confirm(response.message);
-                    if(isAccepted){
+                    if (isAccepted) {
                         window.open("https://github.com/login/oauth/authorize?client_id=d7afcf2bdb1c1f438cdd&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
                         window.localStorage.setItem("closable", true);
                     }
-                }else {
+                } else {
                     alert(response.message);
                 }
             }
         },
-        dataType:"json"
+        dataType: "json"
     });
+}
+
+//展开二级回复
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-" + id);
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse) {
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    } else {
+        comments.addClass("in");
+        e.setAttribute("data-collapse", "in");
+        e.classList.add("active");
+    }
 }
