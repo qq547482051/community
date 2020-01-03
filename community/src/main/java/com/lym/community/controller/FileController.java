9420e2f1b9b6372977e91a967e112c1f4612d1ee
lym <547482051@qq.com>
2020-01-03 11:11:11 +0800

    添加图片上传

---
+++ community/src/main/java/com/lym/community/controller/FileController.java
@@ -0,0 +1,18 @@
+package com.lym.community.controller;
+
+import com.lym.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/loading.gif");
+        return fileDTO;
+    }
+}
